54ec8a1d44e32132ce92d1fde31cfc51f1de4bda
Iliya Zhechev <i4ko95@gmail.com>
2017-03-16 20:04:31 +0200

    Generator class

---
+++ scripts/engine/generator.js
@@ -0,0 +1,22 @@
+class Generator {
+
+    constructor({ set, cls, config = {} } = {}) {
+        this.cls = cls;
+        this.config = config;
+        this.set = set;
+    }
+
+    single() {
+        let result = new type(this.config);
+        for (let name in this.set) {
+            result[name] = this.set[name]();
+        }
+
+        return result;
+    }
+
+    make(size = 1) {
+        return Array.from(Array(size).keys()).map(() => this.single());
+    }
+
+}
