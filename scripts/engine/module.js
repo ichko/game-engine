464cae6ee45baa7ab9bfa1b24c7430a32f347637
Iliya Zhechev <i4ko95@gmail.com>
2017-03-16 20:49:45 +0200

    Game module

---
+++ scripts/engine/module.js
@@ -0,0 +1,42 @@
+let Game = new class Module {
+
+    constructor() {
+        this.definitions = [];
+        this.container = {};
+    }
+
+    load() {
+        this.definitions.forEach(({ single, executor }) => {
+            let cls = executor(this.container);
+            let name = cls.name;
+            cls = (single ? new cls() : cls);
+            this.container[name] = cls;
+        });
+
+        return this;
+    }
+
+    defineSingle(executor) {
+        this.definitions.push({ single: true, executor });
+        return this;
+    }
+
+    define(executor) {
+        this.definitions.push({ single: false, executor });
+        return this;
+    }
+
+    useAll(global) {
+        for (let name in this.definitions) {
+            global[name] = this.definitions;
+        }
+
+        return this;
+    }
+
+    use(dependencies = [], global) {
+        dependencies.forEach(dependency => global[dependency] = this.definitions[dependency]);
+        return this;
+    }
+
+}
--- scripts/engine/vector.js
+++ scripts/engine/vector.js
@@ -1,4 +1,4 @@
-class Vector {
+Game.define(() => class Vector {
 
     constructor(x = 0, y = 0) {
         this.x = x;
@@ -25,4 +25,4 @@ class Vector {
         return new Vector(this.x * vector.x, this.y * vector.y);
     }
 
-}
+});
