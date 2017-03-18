e7663d9dcf653608c02c098fc4309d042944b585
Iliya Zhechev <i4ko95@gmail.com>
2017-03-18 19:39:10 +0200

    Colorful theme

--- scripts/main.js
+++ scripts/main.js
@@ -20,28 +20,30 @@ let camera = new SpringyVector({
 let circleGenerator = new Generator(({ color, size = 20 }) => ({
     cls: Circle,
     set: {
-        position: () => Vector.random(-width, width, -20, 20),
+        position: () => Vector.random(-width, width, -800, 800),
         radius: () => _.random(1, size),
         color: () => color
     }
 }));
 
 let world = new Parallax(() => camera.position)
-    .addLayer({ depth: 1.5, objects: circleGenerator.make(250, { color: 'rgba(200, 0, 100, 0.4)', size: 5 }) })
-    .addLayer({ depth: 1.1, objects: circleGenerator.make(150, { color: 'rgba(200, 0, 100, 0.6)', size: 20 }) })
+    .addLayer({ depth: 1.5, objects: circleGenerator.make(250, { color: 'rgba(50, 100, 200, 0.8)', size: 2 }) })
+    .addLayer({ depth: 1.25, objects: circleGenerator.make(150, { color: 'rgba(240, 120, 0, 0.8)', size: 8 }) })
     .addLayer({ name: 'player layer', objects: [ player ] })
-    .addLayer({ depth: 0.9, objects: circleGenerator.make(150, { color: 'rgba(200, 0, 100, 0.8)', size: 50 }) })
-    .addLayer({ depth: 0.5, objects: circleGenerator.make(80, { color: 'rgba(200, 0, 100, 0.6)', size: 100 }) })
+    .addLayer({ depth: 0.85, objects: circleGenerator.make(150, { color: 'rgba(0, 200, 100, 0.8)', size: 16 }) })
+    .addLayer({ depth: 0.5, objects: circleGenerator.make(80, { color: 'rgba(220, 0, 100, 0.8)', size: 32 }) })
     .add(camera);
 
+let time = 0;
 (function animation() {
+    time++;
     renderer.clear();
     world.render(renderer);
     world.update();
     io.callHandlers();
 
-    player.position.x++;
-    player.position.y += Math.sin(player.position.x / 20) * 2;
+    player.position.x += Math.sin(time / 100) * 2;
+    player.position.y += Math.sin(time / 20) * 2;
 
     requestAnimationFrame(animation);
 })();
