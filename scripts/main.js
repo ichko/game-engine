464804dac11cbd8edc375f16152c882f7f2d3e76
Iliya Zhechev <i4ko95@gmail.com>
2017-03-18 19:24:41 +0200

    Example refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -10,34 +10,28 @@ $Module.load().useIn(window);
 let io = new IO();
 let renderer = new Renderer(ctx, width, height);
 
-let player = new Circle({ radius: 15, color: '#ff9' });
+let player = new Circle({ position: new Vector(-600, 100), radius: 15, color: '#ff9' });
 let camera = new SpringyVector({
-    position: new Vector(-300, 0),
+    position: new Vector(-600, 250),
     elasticity: 0.01,
-    target: () => player.position
+    target: () => player.position.add(new Vector(300, 10))
 });
 
 let circleGenerator = new Generator(({ color, size = 20 }) => ({
     cls: Circle,
     set: {
-        position: () => Vector.random(-width, width, -height, height),
-        radius: () => _.random(5, size),
+        position: () => Vector.random(-width, width, -20, 20),
+        radius: () => _.random(1, size),
         color: () => color
     }
 }));
 
 let world = new Parallax(() => camera.position)
-    .addLayer({
-        name: 'foreground.far',
-        depth: 1,
-        objects: circleGenerator.make(150, { color: '#906' })
-    })
-    .addLayer({
-        name: 'foreground.near',
-        depth: 0.5,
-        objects: circleGenerator.make(150, { color: '#603', size: 50 })
-    })
+    .addLayer({ depth: 1.5, objects: circleGenerator.make(250, { color: 'rgba(200, 0, 100, 0.4)', size: 5 }) })
+    .addLayer({ depth: 1.1, objects: circleGenerator.make(150, { color: 'rgba(200, 0, 100, 0.6)', size: 20 }) })
     .addLayer({ name: 'player layer', objects: [ player ] })
+    .addLayer({ depth: 0.9, objects: circleGenerator.make(150, { color: 'rgba(200, 0, 100, 0.8)', size: 50 }) })
+    .addLayer({ depth: 0.5, objects: circleGenerator.make(80, { color: 'rgba(200, 0, 100, 0.6)', size: 100 }) })
     .add(camera);
 
 (function animation() {
@@ -47,7 +41,7 @@ let world = new Parallax(() => camera.position)
     io.callHandlers();
 
     player.position.x++;
-    player.position.y = Math.sin(player.position.x / 20) * 30;
+    player.position.y += Math.sin(player.position.x / 20) * 2;
 
     requestAnimationFrame(animation);
 })();
