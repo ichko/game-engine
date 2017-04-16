121ba524e58a7d5921dbf43447f3bd91fb041218
Iliya Zhechev <i4ko95@gmail.com>
2017-04-16 18:31:15 +0300

    ES5 Build

---
+++ build/config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+App.defineSingle(function () {
+    return function Config() {
+        _classCallCheck(this, Config);
+
+        this.colors = ['#6f6', '#f66', '#66f', '#ff3', '#3ff', '#f3f'];
+    };
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9jb25maWcuanMiXSwibmFtZXMiOlsiQXBwIiwiZGVmaW5lU2luZ2xlIiwiY29sb3JzIl0sIm1hcHBpbmdzIjoiOzs7O0FBQUFBLElBQUlDLFlBQUosQ0FBaUI7QUFBQSxXQUViLGtCQUFjO0FBQUE7O0FBQ1YsYUFBS0MsTUFBTCxHQUFjLENBQUMsTUFBRCxFQUFTLE1BQVQsRUFBaUIsTUFBakIsRUFBeUIsTUFBekIsRUFBaUMsTUFBakMsRUFBeUMsTUFBekMsQ0FBZDtBQUNILEtBSlk7QUFBQSxDQUFqQiIsImZpbGUiOiJjb25maWcuanMiLCJzb3VyY2VzQ29udGVudCI6WyJBcHAuZGVmaW5lU2luZ2xlKCgpID0+IGNsYXNzIENvbmZpZyB7XHJcblxyXG4gICAgY29uc3RydWN0b3IoKSB7XHJcbiAgICAgICAgdGhpcy5jb2xvcnMgPSBbJyM2ZjYnLCAnI2Y2NicsICcjNjZmJywgJyNmZjMnLCAnIzNmZicsICcjZjNmJ107XHJcbiAgICB9XHJcblxyXG59KTsiXX0=
