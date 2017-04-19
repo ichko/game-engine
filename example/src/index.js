6a3cf8c08799cea8fcc144823b96c9a746275395
Iliya Zhechev <i4ko95@gmail.com>
2017-04-20 01:54:46 +0300

    Cleanup

--- example/src/index.js
+++ example/src/index.js
@@ -11,54 +11,58 @@ import { Environment } from './environment';
 import { Controller } from './controller';
 
 
-let animationFrame = undefined;
+let init = (canvas) => {
 
-// window.onresize = init;
-// function init() {
-
-let [ ctx, width, height ] = (() => {
-    let canvas = document.getElementById('canvas');
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
+    let [ width, height ] = [ innerWidth, innerHeight ];
+    canvas.width = width;
+    canvas.height = height;
     let ctx = canvas.getContext('2d');
-    ctx.translate(innerWidth / 2, innerHeight / 2);
+    ctx.translate(width / 2, height / 2);
     ctx.scale(1.05, -1.05);
 
-    return [ ctx, innerWidth, innerHeight ];
-})();
+
+    let io = new IO(width, height);
+    let renderer = new CanvasRenderer(ctx, width, height);
+    let scene = new Scene();
+    let engine = new Engine(renderer, scene);
+
+    let controller = new Controller(io, width, height);
+    let player = new Player(controller);
+    let camera = new SpringyVector({
+        position: new Vector(0, 50), elasticity: 0.08, damping: 0.3,
+        target: () => player.position.copy().add(player.velocity.copy().scale(3))
+    });
+
+    let parallax = new Parallax(camera.position);
+    let environment = new Environment(camera, width, height);
+
+    io.onMouse(() => player.speed = 5, () => player.speed = 0);
+    environment.container.forEach(layer => parallax.addLayer({ depth: layer.depth, objects: layer.objects }));
+    parallax.addLayer({ objects: [player, new AsteroidField(player, Math.max(width, height))] });
+    scene.add(parallax, controller, camera);
 
 
-let io = new IO(width, height);
-let renderer = new CanvasRenderer(ctx, width, height);
-let scene = new Scene();
-let engine = new Engine(renderer, scene);
+    cancelAnimationFrame(animationFrame);
+    (function animation() {
+        engine.clear().render().update();
+        environment.outOfBounds();
 
-let controller = new Controller(io, width, height);
-let player = new Player(controller);
-let camera = new SpringyVector({
-    position: new Vector(0, 50), elasticity: 0.08, damping: 0.3,
-    target: () => player.position.copy().add(player.velocity.copy().scale(3))
-});
+        parallax.zoom = 50 / (player.velocity.length() * 3 + 30);
 
-let parallax = new Parallax(camera.position);
-let environment = new Environment(camera, width, height);
+        animationFrame = requestAnimationFrame(animation);
+    })();
+
+}
 
 
-io.onMouse(() => player.speed = 5, () => player.speed = 0);
-environment.container.forEach(layer => parallax.addLayer({ depth: layer.depth, objects: layer.objects }));
-parallax.addLayer({ objects: [player, new AsteroidField(player, Math.max(width, height))] });
-scene.add(parallax, controller, camera);
+let animationFrame = undefined,
+    canvas = document.getElementById('canvas');
 
-
-cancelAnimationFrame(animationFrame);
-(function animation() {
-    engine.clear().render().update();
-    environment.outOfBounds();
-
-    parallax.zoom = 50 / (player.velocity.length() * 3 + 30);
-
-    animationFrame = requestAnimationFrame(animation);
-})();
-
+init(canvas);
+// window.onresize = () => init(canvas);
+// canvas.onclick = () => {
+//     (canvas.requestFullScreen ||
+//         canvas.webkitRequestFullScreen ||
+//         canvas.mozRequestFullScreen ||
+//         canvas.msRequestFullScreen).call(canvas);
 // }
-// init();
