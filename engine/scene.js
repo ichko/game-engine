b45f556eb1d958e10ce55fe0d12063931734a32d
Iliya Zhechev <i4ko95@gmail.com>
2017-04-24 14:12:19 +0000

    Scene render and setup

--- engine/scene.js
+++ engine/scene.js
@@ -5,18 +5,28 @@ export class Scene {
     }
 
     add(...objects) {
+        objects.forEach(objects => {
+             if (typeof objects.setup !== 'undefined') {
+                objects.setup();
+            }
+        });
+
         this.container = this.container.concat(objects);
         return this;
     }
 
     render(renderer) {
-        this.container.forEach(object => object.render(renderer));
+        this.container.forEach(object => {
+            if (typeof object.render !== 'undefined') {
+                object.render(renderer);
+            }
+        });
     }
 
     update(ctx) {
         this.container = this.container.filter(object => {
             object.update(ctx);
-            if (typeof object.alive !== "undefined") {
+            if (typeof object.alive !== 'undefined') {
                 return object.alive();
             }
 
